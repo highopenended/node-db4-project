7ec7616cb1bfd9d0cd312e5756e5f27927cd7429
Matt Glenn <highopenended@gmail.com>
2024-11-30 13:00:17 -0500

    wip

--- api/recipes/recipes-router.js
+++ api/recipes/recipes-router.js
@@ -1,8 +1,19 @@
 const router = require("express").Router();
+const Recipe = require('./recipes-model')
+
+router.get('/:recipe_id',(req,res,next)=>{
+    console.log("id: ", req.params.recipe_id)
+    Recipe.getRecipeById(req.params.recipe_id)
+        .then(resource=>{
+            res.status(200).json(resource)
+        })
+        .catch(next)
+})
 
 router.use("*", (req, res) => {
     res.status(404).json({ error: "Route not found" });
 });
+
 
 router.use((err, req, res, next) => {
     res.status(500).json({
